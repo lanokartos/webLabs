db57da4729e345c97f340fa5a55b6721e77f432e
lanokartos <dimamamonets@gmail.com>
2026-09-13 22:13:00 +0300

    added .gitignore, eslint, configuration

---
+++ eslint.config.mjs
@@ -0,0 +1,17 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser
+      }
+    },
+    rules: {
+      "no-unused-vars": "error",
+      "no-undef": "error"
+    }
+  }
+];
